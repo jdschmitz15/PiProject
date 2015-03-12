6ab676c4a28175d39447527ff94121c26abc81cb
jschmitz <jdschmitz@yahoo.com>
2015-03-12 13:22:00 -0400

    Up it goes

--- temphumidity/models.py
+++ temphumidity/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class HTData(models.Model):
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
     temp = models.DecimalField(decimal_places=5, max_digits=10)
     humidity = models.DecimalField(decimal_places=5,max_digits=10)
     deviceid = models.IntegerField()
