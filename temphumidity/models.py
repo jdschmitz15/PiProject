ce6560dfa1152f004dbe882af230513e65f8bdfe
jschmitz <jdschmitz@yahoo.com>
2015-03-12 13:51:48 -0400

    Up it goes

--- temphumidity/models.py
+++ temphumidity/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class HTData(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     temp = models.DecimalField(decimal_places=15, max_digits=20)
-    humidity = models.DecimalField(decimal_places=150,max_digits=20)
+    humidity = models.DecimalField(decimal_places=15,max_digits=20)
     deviceid = models.IntegerField()
     def __unicode__(self):              # __unicode__ on Python 2
         return unicode(self.date)
